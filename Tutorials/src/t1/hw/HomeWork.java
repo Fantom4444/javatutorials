629d5a0ad390af61069b275b8fbb1ae661377e2a
Fantom <astapov_80@list.ru>
2016-01-31 18:39:55 +0300

    Tutorial1

---
+++ Tutorials/src/t1/hw/HomeWork.java
@@ -0,0 +1,10 @@
+package t1.hw;
+
+/**
+ * Created by Fantom on 31.01.2016.
+ */
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("mama");
+    }
+}
